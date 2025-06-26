8ba366b0eb053300d1c81aea4328ef3d73bc0c4c
Pallabi Mondal <150214037+pallabisneha@users.noreply.github.com>
2025-06-27 01:25:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
-import os
-os.system("pip install tensorflow==2.13.0")
+# import os
+# os.system("pip install tensorflow==2.13.0")
 
-from flask import Flask, render_template
-from final_pred import run_model  # Create a function in final_pred.py to call
+# from flask import Flask, render_template
+# from final_pred import run_model  # Create a function in final_pred.py to call
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return run_model()  # Or return render_template('index.html') if using templates
+# @app.route('/')
+# def index():
+#     return run_model()  # Or return render_template('index.html') if using templates
 
-if __name__ == "__main__":
-    app.run()
+# if __name__ == "__main__":
+#     app.run()
